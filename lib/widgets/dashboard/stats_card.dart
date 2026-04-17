6739c55f8433887942f91d398339137680672ec5
Med Elmostapha <dedijekani@gmail.com>
2026-04-17 12:35:16 +0000

    feat(ui/dashboard): add stats cards and grid layout

---
+++ lib/widgets/dashboard/stats_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_de_stock_flutter/core/theme/app_colors.dart';
+import 'package:intl/intl.dart';
+
+class StatsCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final double value;
+  final IconData icon;
+  final Color iconbg;
+
+  const StatsCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.value,
+    required this.icon,
+    required this.iconbg,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(
+        minWidth: 140,
+        minHeight: 80,
+        maxWidth: 160,
+        maxHeight: 90,
+      ),
+
+      margin: const EdgeInsets.all(8),
+      padding: const EdgeInsets.all(14),
+      decoration: BoxDecoration(
+        color: AppColors.white,
+        borderRadius: BorderRadius.circular(16),
+      ),
+
+      child: Row(
+        children: [
+          Container(
+            width: 40,
+            height: 40,
+            decoration: BoxDecoration(
+              color: iconbg,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Icon(icon, color: AppColors.white),
+          ),
+
+          const SizedBox(width: 12),
+
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                NumberFormat.compact().format(value),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+                style: const TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(title, style: const TextStyle(color: Colors.grey)),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
