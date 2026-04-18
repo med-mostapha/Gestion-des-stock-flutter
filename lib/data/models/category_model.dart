066dabd578d411e840229d7937a7b273f8a705cd
Med Elmostapha <dedijekani@gmail.com>
2026-04-18 20:13:22 +0000

    feat(dummy): enrich categories with description field

--- lib/data/models/category_model.dart
+++ lib/data/models/category_model.dart
@@ -1,6 +1,7 @@
 class Category {
   final String id;
   final String name;
+  final String? description;
 
-  Category({required this.id, required this.name});
+  Category({required this.id, required this.name, this.description});
 }
