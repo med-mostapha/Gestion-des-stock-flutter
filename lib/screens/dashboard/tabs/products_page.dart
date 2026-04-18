c927e6c09b496ded0bde7234c3d177625b793df8
Med Elmostapha <dedijekani@gmail.com>
2026-04-18 21:25:05 +0000

    feat(products): improve products page with search and product card integration

--- lib/screens/dashboard/tabs/products_page.dart
+++ lib/screens/dashboard/tabs/products_page.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_de_stock_flutter/data/models/product_model.dart';
 import 'package:gestion_de_stock_flutter/data/repositories/product_repository.dart';
+import 'package:gestion_de_stock_flutter/widgets/products/product_card.dart';
 import 'package:gestion_de_stock_flutter/widgets/ui/app_search_bar.dart';
 
-final repo = ProductRepository();
-
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget {
   const ProductsPage({super.key});
 
   @override
+  State<ProductsPage> createState() => _ProductsPageState();
+}
+
+class _ProductsPageState extends State<ProductsPage> {
+  final ProductRepository repository = ProductRepository();
+
+  late List<Product> products;
+  List<Product> filtered = [];
+
+  @override
+  void initState() {
+    super.initState();
+    products = repository.getAllProducts();
+    filtered = products;
+  }
+
+  void search(String query) {
+    setState(() {
+      filtered = products
+          .where((p) => p.name.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(10),
-      child: SearchBarApp(items: []),
+    return Column(
+      children: [
+        SearchBarApp(onChanged: search),
+
+        Expanded(
+          child: ListView.separated(
+            padding: const EdgeInsets.all(12),
+            itemCount: filtered.length,
+            itemBuilder: (context, index) {
+              return ProductCard(product: filtered[index]);
+            },
+            separatorBuilder: (context, index) {
+              return const SizedBox(height: 12);
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- lib/widgets/ui/app_search_bar.dart
+++ lib/widgets/ui/app_search_bar.dart
@@ -1,69 +1,24 @@
 import 'package:flutter/material.dart';
 
 class SearchBarApp extends StatefulWidget {
-  final List<String> items;
+  final Function(String query) onChanged;
 
-  const SearchBarApp({super.key, required this.items});
+  const SearchBarApp({super.key, required this.onChanged});
 
   @override
   State<SearchBarApp> createState() => _SearchBarAppState();
 }
 
 class _SearchBarAppState extends State<SearchBarApp> {
-  late TextEditingController controller;
-  List<String> filteredItems = [];
-
-  @override
-  void initState() {
-    super.initState();
-    controller = TextEditingController();
-    filteredItems = widget.items;
-  }
-
-  void onSearch(String query) {
-    final results = widget.items.where((item) {
-      return item.toLowerCase().contains(query.toLowerCase());
-    }).toList();
-
-    setState(() {
-      filteredItems = results;
-    });
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SearchBar(
-          controller: controller,
-          padding: const WidgetStatePropertyAll(
-            EdgeInsets.symmetric(horizontal: 16),
-          ),
-
-          leading: const Icon(Icons.search),
-          hintText: "Search...",
-          onChanged: onSearch,
-        ),
-
-        const SizedBox(height: 10),
-
-        Expanded(
-          child: filteredItems.isEmpty
-              ? const Center(child: Text("No results found"))
-              : ListView.builder(
-                  itemCount: filteredItems.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(title: Text(filteredItems[index]));
-                  },
-                ),
-        ),
-      ],
+    return SearchBar(
+      controller: controller,
+      hintText: "Search...",
+      leading: const Icon(Icons.search),
+      onChanged: widget.onChanged,
     );
   }
 }
