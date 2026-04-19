dac5733da159c0d7401965c624360b9b33c5ae6c
Med Elmostapha <dedijekani@gmail.com>
2026-04-19 20:15:07 +0000

    feat(settings): add settings page with dark mode toggle and logout button

--- lib/screens/dashboard/tabs/settings_page.dart
+++ lib/screens/dashboard/tabs/settings_page.dart
@@ -1,25 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_de_stock_flutter/routes/app_routes.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: MaterialButton(
-        onPressed: () {
-          print("pressed");
-        },
-        onLongPress: () => print("login press..."),
-        color: Colors.blue,
-        minWidth: 300,
-        height: 40,
-        textColor: Colors.white,
-        padding: EdgeInsets.symmetric(vertical: 20),
+  State<SettingsPage> createState() => _SettingsPage();
+}
 
-        child: const Text("click me"),
-      ),
+class _SettingsPage extends State<SettingsPage> {
+  int i = 0;
+  bool darkMode = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(10),
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 226, 226, 226),
+                spreadRadius: 0.5,
+                blurRadius: 2,
+                offset: Offset(1, 1),
+              ),
+            ],
+            borderRadius: BorderRadius.circular(8),
+          ),
+          child: SwitchListTile(
+            title: Text(
+              "Darck Mode",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+
+            value: darkMode,
+            activeTrackColor: Colors.green,
+            inactiveTrackColor: Colors.grey,
+            activeThumbColor: Colors.white,
+            inactiveThumbColor: Colors.white,
+            onChanged: (val) {
+              setState(() {
+                darkMode = val;
+              });
+            },
+          ),
+        ),
+        MaterialButton(
+          minWidth: 100,
+          height: 40,
+          color: Colors.red[400],
+
+          onPressed: () {
+            Navigator.pushReplacementNamed(context, AppRoutes.login);
+          },
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 5,
+            children: [
+              Text(
+                "Logout",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+              Icon(Icons.logout, color: Colors.white, size: 18),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
