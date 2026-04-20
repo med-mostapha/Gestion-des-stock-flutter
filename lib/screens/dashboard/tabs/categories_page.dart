35b9a3f1d8cfb7c36b659e641002d3b0a9362491
Med Elmostapha <dedijekani@gmail.com>
2026-04-20 18:52:55 +0000

    feat(categories): improve categories listing and navigation flow

--- lib/screens/dashboard/tabs/categories_page.dart
+++ lib/screens/dashboard/tabs/categories_page.dart
@@ -1,34 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:gestion_de_stock_flutter/core/theme/app_colors.dart';
+import 'package:gestion_de_stock_flutter/data/models/category_model.dart';
 import 'package:gestion_de_stock_flutter/data/repositories/category_repository.dart';
+import 'package:gestion_de_stock_flutter/screens/dashboard/tabs/add_category_page.dart';
 import 'package:gestion_de_stock_flutter/widgets/categories/category_card.dart';
 
-// ignore: must_be_immutable
-class CategoriesPage extends StatelessWidget {
-  CategoriesPage({super.key});
+class CategoriesPage extends StatefulWidget {
+  const CategoriesPage({super.key});
 
+  @override
+  State<CategoriesPage> createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<CategoriesPage> {
   final CategoryRepository repository = CategoryRepository();
+
+  List<Category> categories = [];
+
+  @override
+  void initState() {
+    super.initState();
+    categories = repository.getAllCategories();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final categories = repository.getAllCategories();
+    return Scaffold(
+      body: categories.isEmpty
+          ? const Center(child: Text("No categoris found"))
+          : ListView.separated(
+              padding: const EdgeInsets.all(12),
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                final category = categories[index];
+                return CategoryCard(
+                  category: category,
+                  onTap: () => debugPrint("Clicked: ${category.name}"),
+                );
+              },
+              separatorBuilder: (context, index) => const SizedBox(height: 12),
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppColors.white,
 
-    return ListView.separated(
-      padding: const EdgeInsets.all(12),
+        onPressed: () async {
+          final newCategory = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const AddCategoryPage()),
+          );
 
-      itemCount: categories.length,
-      itemBuilder: (context, index) {
-        final category = categories[index];
-
-        return CategoryCard(
-          category: category,
-          onTap: () {
-            debugPrint("Clicked: ${category.name}");
-          },
-        );
-      },
-      separatorBuilder: (context, index) {
-        return const SizedBox(height: 12);
-      },
+          if (newCategory != null && newCategory is Category) {
+            setState(() {
+              categories.add(newCategory);
+            });
+          }
+        },
+        child: const Icon(Icons.add, color: Colors.black),
+      ),
     );
   }
 }
