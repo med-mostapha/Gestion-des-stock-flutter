7f2c90645953b0c295e6e811c9c948c15362af01
Med Elmostapha <dedijekani@gmail.com>
2026-04-27 19:53:51 +0000

    feat: add ProductProvider and CategoryProvider with isLoading and error state

---
+++ lib/providers/category_provider.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_de_stock_flutter/data/repositories/dummy_category_repository.dart';
+import '../data/models/category_model.dart';
+import '../data/repositories/category_repository.dart';
+
+class CategoryProvider extends ChangeNotifier {
+  final CategoryRepository _repository = DummyCategoryRepository();
+
+  List<Category> _categories = [];
+  bool _isLoading = false;
+  String? _error;
+
+  List<Category> get categories => _categories;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+
+  Future<void> loadCategories() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      await Future.delayed(
+        const Duration(seconds: 2),
+      ); // simulator don't forgat to delete this.
+      _categories = await _repository.getAllCategories();
+    } catch (e) {
+      _error = "Failed to load categories";
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void addCategory(Category category) {
+    _categories.add(category);
+    notifyListeners();
+  }
+
+  void updateCategory(String id, Category updatedCategory) {
+    final index = _categories.indexWhere((c) => c.id == id);
+    if (index != -1) {
+      _categories[index] = updatedCategory;
+      notifyListeners();
+    }
+  }
+
+  void deleteCategory(String id) {
+    _categories.removeWhere((c) => c.id == id);
+    notifyListeners();
+  }
+}
---
+++ lib/providers/product_provider.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:gestion_de_stock_flutter/data/repositories/dummy_product_repository.dart';
+import '../data/models/product_model.dart';
+import '../data/repositories/product_repository.dart';
+
+class ProductProvider extends ChangeNotifier {
+  final ProductRepository _repository = DummyProductRepository();
+
+  List<Product> _products = [];
+  bool _isLoading = false;
+  String? _error;
+
+  List<Product> get products => _products;
+  bool get isLoading => _isLoading;
+  String? get error => _error;
+
+  Future<void> loadProducts() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      await Future.delayed(
+        const Duration(seconds: 2),
+      ); // simulator I will delete this latter
+      _products = await _repository.getAllProducts();
+    } catch (e) {
+      _error = "Failed to load products";
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  void addProduct(Product product) {
+    _products.add(product);
+    notifyListeners();
+  }
+
+  // this methode not work now because we use Dummy data
+  void updateProduct(String id, Product updatedProduct) {
+    final index = _products.indexWhere((p) => p.id == id);
+    if (index != -1) {
+      _products[index] = updatedProduct;
+      notifyListeners();
+    }
+  }
+
+  void deleteProduct(String id) {
+    _products.removeWhere((p) => p.id == id);
+    notifyListeners();
+  }
+
+  List<Product> search(String query) {
+    return _products
+        .where((p) => p.name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+  }
+}
