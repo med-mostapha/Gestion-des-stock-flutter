499c42c615c7c291348558862cecf7cffd92c652
Med Elmostapha <dedijekani@gmail.com>
2026-04-27 19:53:58 +0000

    feat: setup MultiProvider in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:gestion_de_stock_flutter/core/theme/app_theme.dart';
+import 'package:gestion_de_stock_flutter/providers/category_provider.dart';
+import 'package:gestion_de_stock_flutter/providers/product_provider.dart';
 import 'package:gestion_de_stock_flutter/routes/app_router.dart';
 import 'package:gestion_de_stock_flutter/routes/app_routes.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +15,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: AppRoutes.main,
-      onGenerateRoute: AppRouter.generateRoute,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => ProductProvider()),
+        ChangeNotifierProvider(create: (_) => CategoryProvider()),
+      ],
+      child: MaterialApp(
+        initialRoute: AppRoutes.main,
+        onGenerateRoute: AppRouter.generateRoute,
 
-      theme: AppTheme.lightTheme,
+        theme: AppTheme.lightTheme,
+      ),
     );
   }
 }
