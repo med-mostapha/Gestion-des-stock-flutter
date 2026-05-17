9ec3f921658b444599055d29f263a08f8aaf35a2
Med Elmostapha <dedijekani@gmail.com>
2026-05-17 12:12:05 +0000

    refactor: add updateProduct method to ProductRepository interface
    - Add updateProduct() to abstract class
    - Enable update operations in repository pattern

--- lib/data/repositories/product_repository.dart
+++ lib/data/repositories/product_repository.dart
@@ -3,5 +3,6 @@ import 'package:gestion_de_stock_flutter/data/models/product_model.dart';
 abstract class ProductRepository {
   Future<List<Product>> getAllProducts();
   Future<void> addProduct(Product product);
+  Future<void> updateProduct(Product product);
   Future<void> deleteProduct(String id);
 }
