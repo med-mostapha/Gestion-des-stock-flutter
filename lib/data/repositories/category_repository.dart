9573eea58a11df81a6918d523f770daa224c1e21
Med Elmostapha <dedijekani@gmail.com>
2026-05-17 12:12:10 +0000

    refactor: add updateCategory method to CategoryRepository interface
    - Add updateCategory() to abstract class
    - Enable update operations in repository pattern

--- lib/data/repositories/category_repository.dart
+++ lib/data/repositories/category_repository.dart
@@ -3,5 +3,6 @@ import 'package:gestion_de_stock_flutter/data/models/category_model.dart';
 abstract class CategoryRepository {
   Future<List<Category>> getAllCategories();
   Future<void> addCategory(Category category);
+  Future<void> updateCategory(Category category);
   Future<void> deleteCategory(String id);
 }
